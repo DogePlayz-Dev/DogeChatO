4dbb2f378a4e40b4907bb19293d6cdfa05772cf8
DogePlayz-Dev <58870118+DogePlayz-Dev@users.noreply.github.com>
2022-04-28 20:28:35 -0700

    made chatting

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ const app = express()
 app.use(express.static('public'))
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
+let blExternalDevices = false // Blacklists all connections that aren't using ChromeOS
+app.use((req,res,next) => {
+    if(blExternalDevices && !(/\bCrOS\b/.test(req.get('User-Agent')))) return res.send('DogeChat has been abandoned.');
+    next()
+})
 
 app.get('/', (req,res) => {
     res.render('index')
@@ -19,10 +24,12 @@ server.listen(3000, () => {
 
 const io = socketio(server, {cors: {origin: '*'}})
 io.on('connection', (socket) => {
-    socket.on('rename', () => {
-
+    let name = socket.id.slice(0,5)
+    let prefix = ''
+    socket.on('rename', (n) => {
+        name = n
     })
-    socket.on('msg', (msgInfo) => {
-
+    socket.on('msg', (content) => {
+        io.sockets.emit('msg', {n: name, c: content})
     })
 })
