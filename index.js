7ee9fd6d5338d33e4880c10ab7ec5a6d53414d8b
DogePlayz-Dev <tjvendingllc@gmail.com>
2022-05-03 18:35:42 +0000

    added port

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 console.clear()
+require('dotenv').config()
 const express = require('express')
 const socketio = require('socket.io')
 const path = require('path')
@@ -8,28 +9,36 @@ app.use(express.static('public'))
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 let blExternalDevices = true // Blacklists all connections that aren't using ChromeOS
-app.use((req,res,next) => {
-    if(blExternalDevices && !(/\bCrOS\b/.test(req.get('User-Agent')))) return res.send('DogeChat has been abandoned.');
+app.use((req, res, next) => {
+    if (blExternalDevices && !(/\bCrOS\b/.test(req.get('User-Agent')))) return res.send('DogeChat has been abandoned.');
     next()
 })
 
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
     res.render('index')
 })
 
 const server = require('http').createServer(app)
-server.listen(3000, () => {
-    console.log('The server is ready.')
-})
+try {
+    server.listen(process.env.PORT || 3000, () => {
+        console.log('The server is ready.')
+        console.log("Listening on Port " + process.env.PORT || 3000)
+    })
+} catch (err) {
+    server.listen(3000, () => {
+        console.log('The server is ready.')
+        console.log("Listening on Port " + process.env.PORT || 3000)
+    })
+}
 
-const io = socketio(server, {cors: {origin: '*'}})
+const io = socketio(server, { cors: { origin: '*' } })
 io.on('connection', (socket) => {
-    let name = socket.id.slice(0,5)
+    let name = socket.id.slice(0, 5)
     let prefix = ''
     socket.on('rename', (n) => {
         name = n
     })
     socket.on('msg', (content) => {
-        io.sockets.emit('msg', {n: name, c: content})
+        io.sockets.emit('msg', { n: name, c: content })
     })
 })
